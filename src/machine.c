521610a478fef29b458b551eee86cab842d7d005
2000ArP <victor.gavrilovici@yahoo.com>
2020-06-09 21:24:08 +0300

    Update machine.c

--- src/machine.c
+++ src/machine.c
@@ -1,14 +1,26 @@
 #include <ijvm.h>
 
+struct
+{
+    word_t *constants;
+    byte_t *text;
+    int textSize;
+    int pc;
+    int constantsSize;
+} binary;
+
+binary *data;
+
 static uint32_t swap_uint32(uint32_t num)
 {
-    return((num>>24)&0xff) | ((num<<8)&0xff0000) | ((num>>8)&0xff00) | ((num<<24)&0xff000000);
+    return ((num>>24)&0xff) | ((num<<8)&0xff0000) | ((num>>8)&0xff00) | ((num<<24)&0xff000000);
 }
 
 int init_ijvm(char *binary_file)
 {
     // Implement loading of binary here
     FILE *fp;
+    binary fileData;
     word_t buffer = 0;
     fp = fopen(binary_file, "r");
     fread(&buffer, sizeof(word_t), 1, fp);      // Reading magic number
@@ -25,13 +37,18 @@ int init_ijvm(char *binary_file)
     buffer = swap_uint32(buffer);
     int constantNo = buffer / 4;
 
-    word_t constants[constantNo];
-    // memset(constants, 0, sizeof(constants));    IDK if needed
+    word_t initConstants[constantNo];
     for(int i = 0; i < constantNo; i++)         // Parsing constant block
     {
         fread(&buffer, sizeof(word_t), 1, fp);
         buffer = swap_uint32(buffer);
-        constants[i] = buffer;
+        initConstants[i] = buffer;
+    }
+    fileData.constants = initConstants;
+
+    for(int i=0; i<constantNo; i++)
+    {
+        fprintf(stderr, "%d\n", fileData.constants[i]);
     }
 
     fread(&buffer, sizeof(word_t), 1, fp);      // Reading text origin
@@ -42,11 +59,11 @@ int init_ijvm(char *binary_file)
     buffer = swap_uint32(buffer);
     int textSize = buffer;
 
-    byte_t iBuffer;
-    for(int i = 0; i < 1; i++)           // Parsing text block
+    byte_t iBuffer, initText[textSize];
+    for(int i = 0; i < 1; i++)                  // Parsing text block
     {
         fread(&iBuffer, sizeof(byte_t), 1, fp);
-        fprintf(stderr, "Instruction: %d\nBIPUSH: %d\n", iBuffer, OP_BIPUSH);
+
     }
 
     return 0;
